0cde80bdc3709ca0ba31b0b9c42e4e2496784738
Samuel <samuelmerces321@gmail.com>
2026-03-05 21:06:24 -0300

    foto corrigida e criação de forEach para adicionar lista de conhecimento e experiências

--- CurriculoJS/script.js
+++ CurriculoJS/script.js
@@ -1,27 +1,57 @@
+
+
 // window.alert("Pagina Carregada")
 window.document.getElementById("btnCarregar").addEventListener("click", () => {
-window.document.getElementById("nome").textContent = dados.nome.toUpperCase();
-// window.document.getElementById("imgUser"). = dados.fotoperfil();
+window.document.getElementById("nome").textContent = dados.nome.toUpperCase()
+//carregamento da image
+ var img = window.document.getElementById("imgUser");
+ img.src = dados.fotoperfil
+ img.style.borderRadius = '100px'
 window.document.getElementById("dtn").textContent = dados.datanascimento;
 window.document.getElementById("em").textContent = dados.email;
 window.document.getElementById("tele").textContent = dados.Telefone;
 window.document.getElementById("git").textContent = dados.github;
 window.document.getElementById("link").textContent = dados.linkedin;
 
+let lista = document.getElementById("listaConhecimentos");
+
+dados.conhecimentos.forEach(conhecimento => {
+
+    let li = document.createElement("li");
+    li.textContent = conhecimento;
+
+    lista.appendChild(li);
+
+});
+
+
+let lista2 = document.getElementById('listaExperiencias');
+
+dados.experienciasProfissionais.forEach(exp =>{
+    let li = document.createElement("li");
+    li.textContent = exp;
+
+    lista2.appendChild(li);
+
+});
+
+
+//Conhecimentos 
+
 
 
 });
 
 const dados = {
-"nome":"samuel de souza",
-"fotoperfil":"/img/fotoperfil.jpg",
+"nome":"samuel Mercês",
+"fotoperfil":"fotodeperfil.jpg",
 "datanascimento":"31/10/2002",
 "email":"samuelmerces321@gmail.com",
 "Telefone":"(11) 7070-7070",
 "linkedin":"in/samuel-mercês-21a23b236",
 "github":"github.com/Samwel-Merces",
 "conhecimentos":["Java","HTML","CSS","Javascript"],
-"experiencias profissionais":["operador de loja","auxiliar admnistrativo"],
+"experienciasProfissionais":["operador de loja","auxiliar admnistrativo"],
 "formacao academica": {
 "nome curso":"ads",
 "instituicao":"senac",
