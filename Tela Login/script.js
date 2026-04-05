d5a75b6487f43f1859d36edde8fbd80c9936dfb0
Samuel <samuelmerces321@gmail.com>
2026-04-04 23:53:50 -0300

    verificação de campo vazio

---
+++ Tela Login/script.js
@@ -0,0 +1,14 @@
+function verificarNome(){
+    const user = document.getElementById("campoUser").value.trim();
+    const pass = document.getElementById("campoPass").value.trim();
+
+    if(!user || !pass){
+        let text = document.getElementById("erro");
+        text.innerHTML = "Campos Vazios!";
+        text.style.color = "red";        
+    }else{
+        let text = document.getElementById("erro");
+        text.innerHTML = "";
+    }
+
+}
